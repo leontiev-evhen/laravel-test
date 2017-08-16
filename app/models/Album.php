da576dc15b9f9a05ea66617ece6c1cefaebfd548
Sam Smith <user11@tc.geeksforless.net>
2017-08-16 19:04:40 +0300

    hello

---
+++ app/models/Album.php
@@ -0,0 +1,11 @@
+<?php
+class Album extends Eloquent
+{
+    protected $table = 'Album';
+    protected $primaryKey = 'AlbumId';
+
+    public function tracks ()
+    {
+        return $this->hasMany('Track', 'AlbumId', 'AlbumId');
+    }
+}
